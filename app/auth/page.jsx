ca17f9f6b274817bf3de512d60a6238d6bd4560d
Michael Holenko <m.holenko@hulbee.com>
2024-03-08 16:48:23 +0200

    style: update login UI

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,50 +1,54 @@
-import { Flex, Box, Card, Heading, Text, TextFieldRoot, TextFieldInput, TextFieldSlot, Button } from "@radix-ui/themes";
+import { Flex, Box, Heading, Text, TextFieldRoot, TextFieldInput, TextFieldSlot, Button } from "@radix-ui/themes";
 import { AvatarIcon, LockClosedIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import styles from "@/styles/auth.module.css";
 
 export default function Auth() {
     return (
-        <Flex direction="column" align="center" justify="center" height="100%">
-            <form action="/auth/signin" method="post">
-                <Card size="4" style={{ maxWidth: "600px" }}>
-                    <Heading>Авторизація</Heading>
-                    <Text>Увійдіть під своїм акаунтом</Text>
+        <main className="full-height">
+            <Flex direction="column" align="center" justify="center" height="100%">
+                <form action="/auth/signin" method="post">
+                    <Box className={styles.container}>
+                        <Box className={styles.header}>
+                            <Heading>Авторизація</Heading>
+                            <Text size="2" color="gray">Увійдіть під своїм акаунтом</Text>
+                        </Box>
 
-                    <Box mb="4">
-                        <Text as="label" htmlFor="email" size="3" weight="medium">E-mail</Text>
-                        <TextFieldRoot>
-                            <TextFieldSlot>
-                                <AvatarIcon width={16} height={16} />
-                            </TextFieldSlot>
-                            <TextFieldInput name="email"
-                                type="email"
-                                size="3"
-                                placeholder="username@cheer.ua"
-                                required />
-                        </TextFieldRoot>
+                        <Box className={styles.field}>
+                            <Text as="label" className={styles.label} htmlFor="email" size="2" weight="medium">
+                                E-mail
+                            </Text>
+
+                            <TextFieldRoot>
+                                <TextFieldSlot>
+                                    <AvatarIcon width={16} height={16} />
+                                </TextFieldSlot>
+                                <TextFieldInput name="email" type="email" size="3"
+                                    placeholder="username@cheer.ua" required />
+                            </TextFieldRoot>
+                        </Box>
+
+                        <Box className={styles.field}>
+                            <Text as="label" className={styles.label} htmlFor="password" size="2" weight="medium">
+                                Пароль
+                            </Text>
+
+                            <TextFieldRoot>
+                                <TextFieldSlot>
+                                    <LockClosedIcon width={16} height={16} />
+                                </TextFieldSlot>
+                                <TextFieldInput name="password" type="password" size="3"
+                                    placeholder="пароль" required />
+                            </TextFieldRoot>
+                        </Box>
+
+                        <Flex className={styles.footer} direction="row" justify="end">
+                            <Button>
+                                Увійти <ArrowRightIcon width={16} height={16} />
+                            </Button>
+                        </Flex>
                     </Box>
-
-                    <Box mb="4">
-                        <Text as="label" htmlFor="password" size="3" weight="medium">Пароль</Text>
-                        <TextFieldRoot>
-                            <TextFieldSlot>
-                                <LockClosedIcon width={16} height={16} />
-                            </TextFieldSlot>
-                            <TextFieldInput
-                                name="password"
-                                type="password"
-                                size="3"
-                                placeholder="пароль"
-                                required />
-                        </TextFieldRoot>
-                    </Box>
-
-                    <Flex direction="row" justify="end">
-                        <Button>
-                            Увійти <ArrowRightIcon width={16} height={16} />
-                        </Button>
-                    </Flex>
-                </Card>
-            </form>
-        </Flex>
+                </form>
+            </Flex>
+        </main>
     );
 }
